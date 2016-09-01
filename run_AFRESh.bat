9d45b16f0b6506f92c3a2302c0fdae22eebed94a
tparidae <Tom.Paridaens@UGent.be>
2016-09-01 11:44:51 +0200

    v1

---
+++ run_AFRESh.bat
@@ -0,0 +1,3 @@
+REM SET JETVMPROP=-Djet.jit.disable.resolution -Djet.gc.heaplimit:0 -Djet.gc.ratio:20 
+cd D:\AFRESh\
+"D:\AFRESh\AFRESh-native\AFRESh.exe" D:\AFRESh\configs\config_encode_chry.properties
